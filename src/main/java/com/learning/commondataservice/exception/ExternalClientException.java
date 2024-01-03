ffc3b4ab759db011f2721e3b6a9446a1475ed61c
Shashith <Shashith.Aravinda@cloudsolutions.com.sa>
2024-01-03 10:25:43 +0530

    added common data service

---
+++ src/main/java/com/learning/commondataservice/exception/ExternalClientException.java
@@ -0,0 +1,15 @@
+package com.learning.commondataservice.exception;
+
+public class ExternalClientException extends RuntimeException {
+
+    private final String errorCode;
+
+    public ExternalClientException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
---
+++ src/test/java/com/learning/commondataservice/CommonDataServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package com.learning.commondataservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonDataServiceApplicationTests {
+
+}
